9fc895bd623f3dafa372d3aa79979d736092d7b0
MARS\kh <x>
2017-05-22 15:42:54 -0700

    Updated comments

--- AngularJS/InjectButton/inject_builtin.js
+++ AngularJS/InjectButton/inject_builtin.js
@@ -1,11 +1,11 @@
 // this all runs during bootstrap & module initialization
-// these are built-in AngularJS services
+// these are built-in AngularJS providers
 var myMod = angular.module('myApp', []); // create our module
 myMod.controller('controllerA', // define controller for our module
                  ['$scope', '$window', // inject two dependencies into the module (via controller)
                     function ($scope, $window)// and provide function using them --> this runs during page initialization!
                     {
-                        $scope.message = "My Module Has Loaded!";
-                        $window.alert($scope.message);
+                        $scope.message = "My Module Has Loaded!";// scope provider
+                        $window.alert($scope.message);// window provider
                     }
                  ]);
