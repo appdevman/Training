4ca40ee722d86f88eba86af8d8b5d1e05862b417
MARS\kh <x>
2017-05-30 13:24:38 -0700

    Example of AngularJS include directive for inline html

---
+++ AngularJS/DirectiveInclude/directive_angular_include.js
@@ -0,0 +1,4 @@
+angular.module('myApp', []). //module 
+  controller('myController', function($scope) { //controller & scope creation
+    $scope.titleBar = "small_title.html";//provider initialization to value
+  });
