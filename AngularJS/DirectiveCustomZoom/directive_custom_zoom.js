5b9f622c7c2be0fa2a67583eb539803fcc48b10e
MARS\kh <x>
2017-06-06 15:02:17 -0700

    Custom Directive Handling Mouse Events

---
+++ AngularJS/DirectiveCustomZoom/directive_custom_zoom.js
@@ -0,0 +1,80 @@
+// NO CONTROLLERS (or scopes) in this module, just two custom directives to manipulate DOM
+// using full jquery library in html src
+// "Implementing Event Handlers in a Custom Directive"
+angular.module('myApp', [])
+.directive('zoomit', function() {
+  return {
+    link: function (scope, elem, attr){
+      var dragging = false;
+      var tolerance = 10;
+      var lastX = 0;
+      elem.on('mousedown', function(event){
+        lastX = event.screenX;
+        event.preventDefault();
+        dragging = true;
+      });
+      elem.on('mouseup', function(){
+        dragging = false;
+      });
+      elem.on('mouseleave', function(){
+        dragging = false;
+      });
+      elem.on('mousemove', function(event){
+        if(dragging){
+          var adjustment = null;
+          if ( event.screenX > lastX+tolerance &&
+              elem.width() < 300){
+            adjustment = 1.1;
+          } else if ( event.screenX < lastX-tolerance &&
+              elem.width() > 100){
+            adjustment = .9;
+          }          
+          if(adjustment){          
+            //requires full jQuery library
+            elem.width(elem.width()*adjustment);
+            elem.height(elem.height()*adjustment);
+            lastX = event.screenX;
+          }
+        }
+      });
+    }
+  };
+})
+.directive('fadeit', function() {
+  return {
+    link: function (scope, elem, attr){
+      var dragging = false;
+      var tolerance = 10;
+      var lastY = 0;
+      elem.on('mousedown', function(event){
+        lastY = event.screenY;
+        event.preventDefault();
+        dragging = true;
+      });
+      elem.on('mouseup', function(){
+        dragging = false;
+      });
+      elem.on('mouseleave', function(){
+        dragging = false;
+      });
+      elem.on('mousemove', function(event){
+        if(dragging){
+          var adjustment = null;
+          var currentOpacity = parseFloat(elem.css("opacity"));
+          if ( event.screenY > lastY+tolerance &&
+              currentOpacity < 1){
+            adjustment = 1.1;
+          } else if ( event.screenY < lastY-tolerance &&
+              currentOpacity > 0.5){
+            adjustment = .9;
+          }
+          if(adjustment){          
+            //requires full jQuery library
+            elem.css("opacity", currentOpacity*adjustment);
+            lastY = event.screenY;
+          }
+        }
+      });
+    }
+  };
+});
