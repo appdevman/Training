b38563da52d8a0ae4ae2d4b4df96fd7c1d0e7969
MARS\kh <x>
2017-10-05 12:50:48 -0700

    Reverse order of characters in given string

---
+++ _Misc/ReverseCharOrderInString/ReverseCharOrderInString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseCharOrderInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "xof kciuq";
+            StringBuilder sb = new StringBuilder();
+            Stack<char> worker = new Stack<char>();
+            
+            foreach(char c in input)
+            {
+                worker.Push(c);                
+            }
+            while (worker.Count > 0)
+            {
+                sb.Append(worker.Pop());
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
