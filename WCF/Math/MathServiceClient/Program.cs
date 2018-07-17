df7977fa1202be0a2e70c3ff188b9811d8a34de5
MARS\kh <x>
2018-07-17 16:25:22 -0700

    WCF Service Library project template in VS2017, async method call.

---
+++ WCF/Math/MathServiceClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathServiceClient.MathServiceReference;
+using System.Threading;
+
+namespace MathServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BasicMathClient proxy = new BasicMathClient())
+            {
+                proxy.Open();
+
+                // add numbers in an async manner, using lambda expression
+                IAsyncResult result = proxy.BeginAdd(2, 3,
+                    ar =>
+                    {
+                        Console.WriteLine("2 +3 = {0}", proxy.EndAdd(ar));
+                    },
+                    null);
+
+                while(!result.IsCompleted)
+                {
+                    Thread.Sleep(200);
+                    Console.WriteLine("Client working...");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WCF/Math/MathWindowsServiceHost/MathWinService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using MathServiceLibrary;
+using System.ServiceModel;
+
+namespace MathWindowsServiceHost
+{
+    public partial class MathWinService : ServiceBase
+    {
+        private ServiceHost myHost;
+
+        public MathWinService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // to be safe
+            if (myHost != null)
+            {
+                myHost.Close();
+            }
+
+            // create host
+            myHost = new ServiceHost(typeof(MathService),
+                        new Uri("http://localhost:8087/MathServiceLibrary"));
+
+            // the ABC's in code (address, binding, contract)
+            //Uri address = new Uri("http://localhost:8087/MathServiceLibrary");
+            //WSHttpBinding binding = new WSHttpBinding();
+            //Type contract = typeof(IBasicMath);
+
+            // add this endpoint
+            //myHost.AddServiceEndpoint(contract, binding, address);
+
+            // Opt in for default endpoints, rather than the manual wire-up above
+            myHost.AddDefaultEndpoints();
+            
+            // open the host
+            myHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            //shut down the host
+            if(myHost != null)
+            {
+                myHost.Close();
+            }
+        }
+    }
+}
